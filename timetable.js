4b25d23a5e0e9a9a94b878a1445af3c1f9fd2346
Jef Lippiatt <jeflippiatt@gmail.com>
2015-02-02 16:14:57 -0500

    Added Timer
    Added the Riot JS timer and some styling for it.

---
+++ timetable.js
@@ -0,0 +1,17 @@
+
+riot.tag('timer', '<p>Seconds Elapsed: { time }</p>', function (opts) {
+    this.time = opts.start || 0;
+
+    this.tick = (function () {
+        this.update({
+            time: ++this.time
+        });
+    }).bind(this);
+
+    var timer = setInterval(this.tick, 1000);
+
+    this.on('unmount', function () {
+        console.info('timer cleared');
+        clearInterval(timer);
+    });
+});
